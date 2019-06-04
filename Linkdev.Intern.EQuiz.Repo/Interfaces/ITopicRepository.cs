00ef42834b32e8e45a5d26499842862d89f69841
Ahmed Sherif <ahmed.sherif.fci@gmail.com>
2019-06-04 16:07:49 +0200

    Adding some features to Topic

--- Linkdev.Intern.EQuiz.Repo/Interfaces/ITopicRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Interfaces/ITopicRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Linkdev.Intern.EQuiz.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace Linkdev.Intern.EQuiz.Repo.Interfaces
 {
     public interface ITopicRepository
     {
+        IEnumerable<Topic> GetTopicsByCreationDate();
+
+        IEnumerable<Topic> GetTopicsByName(bool ascending);
+
+        IEnumerable<Topic> FilterTopicsByName(string name);
+
+        IEnumerable<Topic> GetTopicsByPage(int pageIndex, int pageSize = 10);
     }
 }
--- Linkdev.Intern.EQuiz.Repo/Repositories/TopicRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Repositories/TopicRepository.cs
@@ -10,8 +10,47 @@ namespace Linkdev.Intern.EQuiz.Repo.Repositories
 {
     public class TopicRepository : Repository<Topic>, ITopicRepository
     {
+        public EQuizContext EQuizContext
+        {
+            get
+            {
+                return Context as EQuizContext;
+            }
+        }
+
         public TopicRepository(EQuizContext context) : base(context)
         {
+        }
+
+        public IEnumerable<Topic> FilterTopicsByName(string name)
+        {
+            return EQuizContext.Topics
+                .Where(t => t.Name.ToLower()
+                .Contains(name.ToLower()))
+                .AsEnumerable();
+        }
+
+        public IEnumerable<Topic> GetTopicsByCreationDate()
+        {
+            return EQuizContext.Topics
+                .OrderByDescending(t => t.CreationDate)
+                .AsEnumerable();
+        }
+
+        public IEnumerable<Topic> GetTopicsByName(bool ascending)
+        {
+            if (ascending)
+                return EQuizContext.Topics.OrderBy(t => t.Name);
+
+            return EQuizContext.Topics.OrderByDescending(t => t.Name);
+        }
+
+        public IEnumerable<Topic> GetTopicsByPage(int pageIndex, int pageSize = 10)
+        {
+            return EQuizContext.Topics
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize)
+                .AsEnumerable();
         }
     }
 }
