fabc724e9ea1ca7804c8f275742196869f511e4f
Waleed.Hegazy <Waleed.Hegazy@WALEED-HEGAZY>
2019-06-16 15:56:12 +0200

    Adding Some Topics Repo Functions - Waleed

--- Linkdev.Intern.EQuiz.Repo/Repositories/QuestionRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Repositories/QuestionRepository.cs
@@ -10,8 +10,74 @@ namespace Linkdev.Intern.EQuiz.Repo.Repositories
 {
     public class QuestionRepository : Repository<Question>, IQuestionRepository
     {
+        public EQuizContext EQuizContext
+        {
+            get
+            {
+                return Context as EQuizContext;
+            }
+        }
+
         public QuestionRepository(EQuizContext context) : base(context)
         {
+        }
+
+        public bool? IsQuestionActive(int id)
+        {
+            var ques = GetByID(id);
+            return ques.IsActive;
+        }
+
+        public bool? IsQuestionUsed(int id)
+        {
+            var ques = GetByID(id);
+            return ques.IsUsed;
+        }
+
+        public override bool? Remove(Question entity)
+        {
+            if (entity != null)
+            {
+                if (IsQuestionUsed(entity.ID) == false && IsQuestionActive(entity.ID) == false)
+                {
+                    var ques = GetByID(entity.ID);
+
+                    ques.IsDeleted = true;
+
+                    ques.Answers.Select(a => { a.IsDeleted = true; return a; }).ToList();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool? EditQuestion(Question question)
+        {
+            if (question != null)
+            {
+                var ques = GetByID(question.ID);
+
+                ques = question;
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool? ChangeCorrectAnswers(int questionId, ICollection<Answer> answers)
+        {
+            if (answers != null)
+            {
+                var ques = GetByID(questionId);
+
+                ques.Answers = answers;
+
+                return true;
+            }
+
+            return false;
         }
     }
 }
--- Linkdev.Intern.EQuiz.Repo/Repositories/TopicRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Repositories/TopicRepository.cs
@@ -8,6 +8,12 @@ using System.Web;
 
 namespace Linkdev.Intern.EQuiz.Repo.Repositories
 {
+    public enum TopicQuestionsState
+    {
+        NoQuestionsAssigned,NoQuestionsUsed,QuestionsUsed
+    }
+
+
     public class TopicRepository : Repository<Topic>, ITopicRepository
     {
         public EQuizContext EQuizContext
@@ -27,7 +33,7 @@ namespace Linkdev.Intern.EQuiz.Repo.Repositories
             return EQuizContext.Topics
                     .Where(t => t.Name.ToLower()
                     .Contains(name.ToLower()))
-                    .Skip((pageIndex - 1) * pageSize)
+                    .Skip((pageIndex - 1) * pageSize) // what if page index = 1 ??
                     .Take(pageSize)
                     .AsEnumerable();
         }
@@ -65,6 +71,59 @@ namespace Linkdev.Intern.EQuiz.Repo.Repositories
                     .Skip((pageIndex - 1) * pageSize)
                     .Take(pageSize)
                     .AsEnumerable();
+        }
+
+
+        // get topic's questions and answers
+        public IEnumerable<Question> GetTopicQuestions(int id)
+        {
+            return GetByID(id)
+                   .Questions
+                   .AsEnumerable();
+        }
+
+        // check topic has questions
+        public bool? TopicHasQuestion(int id)
+        {
+            if (GetByID(id).Questions.Any() )
+                return true;
+            else
+                return false;                   
+        }
+
+        // check topic has questions used
+        public bool? TopicHasQuestionUsed(int id)
+        {
+            if(GetByID(id).Questions.Any(q => q.IsUsed))
+                return true;
+            else
+                return false;
+        }
+
+        // change name
+        public bool? ChangeTopicName(int id, string name)
+        {
+            var topic = GetByID(id);
+            if (topic == null)
+                return false;
+            else
+            {
+               topic.Name = name;
+                return true;
+            }
+        }
+
+
+
+        // delete topic " with all req. conds."
+        public TopicQuestionsState DeleteTopicStates(int id)
+        {
+            if (TopicHasQuestion(id) == false)
+                return TopicQuestionsState.NoQuestionsAssigned;
+            else if (TopicHasQuestionUsed(id) == false)
+                return TopicQuestionsState.NoQuestionsUsed;
+            else
+                return TopicQuestionsState.QuestionsUsed;
         }
     }
 }
