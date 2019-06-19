f36be6c08139842d02a16fdb5f101841d7b5a0d1
AHMED.SHERIF <AHMED.SHERIF@AHMED-SHERIF>
2019-06-19 18:34:54 +0200

    add methods to quiz repo

--- Linkdev.Intern.EQuiz.Repo/Interfaces/IQuizRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Interfaces/IQuizRepository.cs
@@ -4,12 +4,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Linkdev.Intern.EQuiz.Data;
 
 namespace Linkdev.Intern.EQuiz.Repo.Interfaces
 {
     public interface IQuizRepository :  IRepository<Quiz>
     {
         IEnumerable<Quiz> GetQuizesByQuestion(int qid);
+
+        // quiz answer ? wlla template answer ?
+        IEnumerable<Answer> GetQuizAnswers(int id);
+
+        bool? AssignEmployeesToQuiz(int id, ICollection<Employee> employees);
+
+        bool? ReleaseQuizFromEmployees(int id, ICollection<Employee> employees);
+
+        bool? AssignEmployeesToReTakeQuiz(int id, ICollection<Employee> employees);
+
+        bool? ExtendExpirationDate(int id, DateTime expirationDate);
     }
 }
--- Linkdev.Intern.EQuiz.Repo/Repositories/QuizRepository.cs
+++ Linkdev.Intern.EQuiz.Repo/Repositories/QuizRepository.cs
@@ -30,5 +30,35 @@ namespace Linkdev.Intern.EQuiz.Repo.Repositories
                     .Select(q => q.Quiz)
                     .AsEnumerable();
         }
+
+        public IEnumerable<Answer> GetQuizAnswers(int id)
+        {
+            return EQuizContext.Answers
+                .Include(a=>a.Question)
+                .Include(a=>a.Question.Questions_Quizes)
+                .Where(a => a.Question.Questions_Quizes
+                .Any(qq => qq.QuizID == id))
+                .AsEnumerable();
+        }
+
+        public bool? AssignEmployeesToQuiz(int id, ICollection<Employee> employees)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ReleaseQuizFromEmployees(int id, ICollection<Employee> employees)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? AssignEmployeesToReTakeQuiz(int id, ICollection<Employee> employees)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool? ExtendExpirationDate(int id, DateTime expirationDate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
